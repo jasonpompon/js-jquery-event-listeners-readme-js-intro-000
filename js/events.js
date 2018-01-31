13c4b05dc675459214b3cc2e10e96da39cbe0410
Jason Pompon <jasonpompon@gmail.com>
2018-01-31 00:18:54 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -7,8 +7,8 @@ function getIt() {
 }
 
 function frameIt() {
-    $('img').on('load', function() {
-        addClass('tasty')
+    $('document').on('load', function() {
+        $('img').addClass('tasty');
     });
 }
 
